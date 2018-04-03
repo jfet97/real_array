f938b635f1e6f03797c944bdc4c9787aa645c63d
jfet97 <andrysimo1997@gmail.com>
2018-04-03 18:07:01 +0200

    source
    .h added

---
+++ sources/real_array.h
@@ -0,0 +1,47 @@
+#ifndef REALARRAY_H
+#define REALARRAY_H
+
+#include <iostream>
+#include <ostream>
+
+namespace jfet
+{
+
+	template<class T>
+	class real_array
+	{
+		private:
+			size_t _size;
+			T *ptr;
+
+		public:
+			
+			// void constructor
+			real_array();
+
+			// delegated constructor
+			real_array(size_t n);
+
+			// no copy - temporary
+			real_array(const real_array&) = delete;
+			real_array& operator = (const real_array&) = delete;
+			
+			
+			// return the numbers of element inserted in the array
+			size_t size() {
+				return _size;
+			}
+
+			// overload for [] operator
+			T& operator[](size_t n);
+
+			// overload for << operator
+			friend std::ostream& operator<<(std::ostream& os, const real_array &a);
+			
+			// destructor
+			~real_array();
+	};
+}
+
+#endif
+
